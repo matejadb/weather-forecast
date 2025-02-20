ae825e3b2e207c6d98f3a8725aecad40f652b2ab
matejadb <92729211+matejadb@users.noreply.github.com>
2025-02-20 16:45:04 +0100

    make fetch async function

--- app.js
+++ app.js
@@ -14,23 +14,25 @@
 	let feelsLike;
 	let description;
 
-	const getUserLocation = () => {
-		fetch(
+	async function getWeather() {
+		const response = await fetch(
 			`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${userInput.value}?key=RSFS74567369DAKB7FHJ8MZ7V`,
 			{ mode: 'cors' }
-		)
-			.then(function (response) {
-				return response.json();
-			})
-			.then(function (response) {
-				address = response.address;
-				temperature = response.currentConditions.temp;
-				feelsLike = response.currentConditions.feelslike;
-				description = response.description;
-				console.log(address);
-				console.log(`Temp: ${temperature}F, Feels Like: ${feelsLike}F`);
-				console.log(description);
-			});
+		);
+		const weatherData = await response.json();
+
+		address = weatherData.address;
+		temperature = weatherData.currentConditions.temp;
+		feelsLike = weatherData.currentConditions.feelslike;
+		description = weatherData.description;
+
+		console.log(address);
+		console.log(`Temp: ${temperature}F, Feels Like: ${feelsLike}F`);
+		console.log(description);
+	}
+
+	const getUserLocation = () => {
+		getWeather();
 	};
 
 	enterButton.addEventListener('click', getUserLocation);
