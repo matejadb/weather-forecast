9a8d8124209dbb2ee905091242350b4f03734246
matejadb <92729211+matejadb@users.noreply.github.com>
2025-02-20 16:40:53 +0100

    Add user input

--- app.js
+++ app.js
@@ -5,24 +5,33 @@
   Descriptions - response.description
 */
 
-let address;
-let temperature;
-let feelsLike;
-let description;
+(function weatherApp() {
+	const enterButton = document.getElementById('get-location');
+	const userInput = document.getElementById('weather-search');
 
-fetch(
-	'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Belgrade?key=RSFS74567369DAKB7FHJ8MZ7V',
-	{ mode: 'cors' }
-)
-	.then(function (response) {
-		return response.json();
-	})
-	.then(function (response) {
-		address = response.address;
-		temperature = response.currentConditions.temp;
-		feelsLike = response.currentConditions.feelslike;
-		description = response.description;
-		console.log(address);
-		console.log(`Temp: ${temperature}F, Feels Like: ${feelsLike}F`);
-		console.log(description);
-	});
+	let address;
+	let temperature;
+	let feelsLike;
+	let description;
+
+	const getUserLocation = () => {
+		fetch(
+			`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${userInput.value}?key=RSFS74567369DAKB7FHJ8MZ7V`,
+			{ mode: 'cors' }
+		)
+			.then(function (response) {
+				return response.json();
+			})
+			.then(function (response) {
+				address = response.address;
+				temperature = response.currentConditions.temp;
+				feelsLike = response.currentConditions.feelslike;
+				description = response.description;
+				console.log(address);
+				console.log(`Temp: ${temperature}F, Feels Like: ${feelsLike}F`);
+				console.log(description);
+			});
+	};
+
+	enterButton.addEventListener('click', getUserLocation);
+})();
