46f853e70862ebd8fa18f2ab6ce3f9f0f46a7705
matejadb <92729211+matejadb@users.noreply.github.com>
2025-02-21 11:22:54 +0100

    Fix unit group changing

--- app.js
+++ app.js
@@ -15,9 +15,27 @@
 	const description = document.getElementById('description');
 	const nowShowing = document.querySelector('.now-showing');
 
-	let metricChange = false;
+	let getCelcius = true;
+	/* temperature.textContent = `${weatherData.currentConditions.temp} \xB0F`;
+				feelsLike.textContent = `${weatherData.currentConditions.feelslike} \xB0F`; */
+	async function getWeatherCelcius() {
+		try {
+			const response = await fetch(
+				`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${userInput.value}?unitGroup=metric&key=RSFS74567369DAKB7FHJ8MZ7V`,
+				{ mode: 'cors' }
+			);
+			const weatherData = await response.json();
 
-	async function getWeather() {
+			address.textContent = weatherData.address;
+			temperature.textContent = `${weatherData.currentConditions.temp} \xB0C`;
+			feelsLike.textContent = `${weatherData.currentConditions.feelslike} \xB0C`;
+			description.textContent = weatherData.description;
+		} catch (error) {
+			alert(`Location doesn't exist`);
+		}
+	}
+
+	async function getWeatherFahrenheit() {
 		try {
 			const response = await fetch(
 				`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${userInput.value}?key=RSFS74567369DAKB7FHJ8MZ7V`,
@@ -25,21 +43,9 @@
 			);
 			const weatherData = await response.json();
 
-			if (metricChange) {
-				let tempC = parseFloat(weatherData.currentConditions.temp);
-				let feelsLikeC = parseFloat(weatherData.currentConditions.feelslike);
-
-				temperature.textContent = `${Math.round(
-					((5 / 9) * (tempC - 32) * 10) / 10
-				)} \xB0C`;
-				feelsLike.textContent = `${Math.floor(
-					((5 / 9) * (feelsLikeC - 32) * 10) / 10
-				)} \xB0C`;
-			} else {
-				temperature.textContent = `${weatherData.currentConditions.temp} \xB0F`;
-				feelsLike.textContent = `${weatherData.currentConditions.feelslike} \xB0F`;
-			}
 			address.textContent = weatherData.address;
+			temperature.textContent = `${weatherData.currentConditions.temp} \xB0F`;
+			feelsLike.textContent = `${weatherData.currentConditions.feelslike} \xB0F`;
 			description.textContent = weatherData.description;
 		} catch (error) {
 			alert(`Location doesn't exist`);
@@ -47,33 +53,21 @@
 	}
 
 	const getUserLocation = () => {
-		getWeather();
+		if (getCelcius) {
+			getWeatherCelcius();
+		} else {
+			getWeatherFahrenheit();
+		}
 	};
 
 	const changeMetric = () => {
-		metricChange = !metricChange;
-		if (metricChange) {
-			nowShowing.textContent = 'Celcius';
-			let tempC = parseFloat(temperature.textContent);
-			let feelsLikeC = parseFloat(feelsLike.textContent);
-
-			temperature.textContent = `${Math.round(
-				((5 / 9) * (tempC - 32) * 10) / 10
-			)} \xB0C`;
-			feelsLike.textContent = `${Math.round(
-				((5 / 9) * (feelsLikeC - 32) * 10) / 10
-			)} \xB0C`;
+		getCelcius = !getCelcius;
+		if (getCelcius) {
+			nowShowing.textContent = `\xB0C	`;
+			getWeatherCelcius();
 		} else {
-			nowShowing.textContent = 'Fahrenheit';
-			let tempF = parseFloat(temperature.textContent);
-			let feelsLikeF = parseFloat(feelsLike.textContent);
-
-			temperature.textContent = `${Math.round(
-				((9 / 5) * tempF + 32 * 10) / 10
-			)} \xB0F`;
-			feelsLike.textContent = `${Math.round(
-				((9 / 5) * feelsLikeF + 32 * 10) / 10
-			)} \xB0F`;
+			nowShowing.textContent = `\xB0F`;
+			getWeatherFahrenheit();
 		}
 	};
 
